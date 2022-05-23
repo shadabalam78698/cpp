c53aeafe1dd5d18cfe1f27481fb0debaa5f2d30e
shadabalam78698 <106025020+shadabalam78698@users.noreply.github.com>
2022-05-23 20:31:20 +0530

    Add files via upload

---
+++ sum.cpp
@@ -0,0 +1,18 @@
+// Lets Find The Sum Of Two Numbers
+
+
+#include<iostream>
+using namespace std;
+
+int main(){
+   
+    int num1, num2;
+    cout<<"Enter The First Number"<<endl;
+    cin>>num1;
+    cout<<"Enter The Second Number"<<endl;
+    cin>>num2;
+    cout<<"The Sum Of The Numbers"<<num1 + num2;
+
+     return 0;
+
+}
