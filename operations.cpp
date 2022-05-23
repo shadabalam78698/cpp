83cd28c1d152ab1152c3b38d08c0a3fc6031c155
shadabalam78698 <106025020+shadabalam78698@users.noreply.github.com>
2022-05-23 20:31:57 +0530

    Add files via upload

---
+++ operations.cpp
@@ -0,0 +1,29 @@
+// Operaters In c++
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int a=4, b=5;
+    cout<<"operations in c++:"<<endl;
+    cout<<"following are the type of operations in c++"<<endl;
+    cout<<"the value of a + b is "<<a + b<<endl;
+    cout<<"the value of a / b " <<a/b<<endl;
+    cout<<"the value of a ++ is " <<a++<<endl;
+    cout<<a;
+    cout<<endl;
+    // assigment operator
+    // comparision operator
+    cout<<"following are the types of comparision operators in c++"<<endl;
+    cout<<"the value of a == b is "<<(a==b)<<endl;
+    cout<<"the value of a <= b is "<<(a<=b)<<endl;
+    cout<<"the value of a >= b is "<<(a>=b)<<endl;
+    cout<<"the value of a > b is "<<(a>b)<<endl;
+    cout<<"following are the logical operator"<<endl;
+    cout<<"the value of this logical and operator ((a==b) && (a<b)) is "<<((a==b) && (a<b))<<endl;
+    cout<<"the value of this logical or operator ((a==b) || (a<b)) is "<<((a==b) || (a<b))<<endl;
+    cout<<"the value of this "<<(!(a==b))<<endl;
+
+
+    return 0;
+}
