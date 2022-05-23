b679c77d8dd3e1b9db681af71c0cc6d8d47ff911
shadabalam78698 <106025020+shadabalam78698@users.noreply.github.com>
2022-05-23 20:32:30 +0530

    Add files via upload

---
+++ switchcase.cpp
@@ -0,0 +1,28 @@
+// Switch Case
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int age;
+    cout<<"tell me your age"<<endl;
+    cin>>age;
+    
+    switch (age)
+    {
+    case 18:
+        cout<<"you are 18"<<endl;
+        break;
+    case 22:
+        cout<<"you are 22"<<endl;
+        break;
+    case 2:
+        cout<<"you are 2"<<endl;
+        break;
+
+    default:
+    cout<<"no special case"<<endl;
+    break;
+    }
+    return 0;
+}
