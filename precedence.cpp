baee61de60164710e448e260230ae063b05143e6
shadabalam78698 <106025020+shadabalam78698@users.noreply.github.com>
2022-05-24 15:27:03 +0530

    Add files via upload

---
+++ precedence.cpp
@@ -0,0 +1,13 @@
+// Operator Precedence
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+int main(){
+   
+    int a=3, b=4;
+    int c =((((a*5) +b)-45)+56);
+    cout<<c;
+
+    return 0;
+}
