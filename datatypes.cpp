7fff84aa3c97f6bcd4184497c6973591f8a2980c
shadabalam78698 <106025020+shadabalam78698@users.noreply.github.com>
2022-05-24 15:26:22 +0530

    Add files via upload

---
+++ datatypes.cpp
@@ -0,0 +1,18 @@
+// Different Types Of Data
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    
+    int a = 4, b = 5;
+    float pi = 3.14;
+    char c = 'u';
+    bool is_true = true;
+    cout<<"this is tutorial 2.\n here the value of a is "<<a<<" .\n the value of b is "<<b;
+    cout<<"\nthe value of pi is "<<pi;
+    cout<<"\n the value of c is "<<c;
+    cout<<is_true;
+
+    return 0;
+}
