58a92cb3f7bbe83b058ed79dacb031159ceb9cf8
shadabalam78698 <106025020+shadabalam78698@users.noreply.github.com>
2022-05-25 19:19:16 +0530

    Add files via upload

---
+++ sizeof.cpp
@@ -0,0 +1,19 @@
+// Using Sizeof
+#include<iostream>
+
+using namespace std;
+
+
+
+int main(){
+   
+
+    float d=34.5f;
+    long double e = 34.5l;
+    cout<<"the size of 34.5 is "<<sizeof(34.5)<<endl;
+    cout<<"the size of 34.5f is "<<sizeof(34.5f)<<endl;
+    cout<<"the size of 34.5L is "<<sizeof(34.5L)<<endl;
+    cout<<"the value of d is "<<d<<endl<<"the value of e is "<<e;
+   
+    return 0;
+}
