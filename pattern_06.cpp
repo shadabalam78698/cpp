97bb4710f2ac162bf40f120b8c29f5701dfc18f9
shadabalam78698 <106025020+shadabalam78698@users.noreply.github.com>
2022-05-29 12:40:18 +0530

    Add files via upload

---
+++ pattern_06.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter Your Number"<<endl;
+    cin>>n;
+for(int i =n; i>0;i--){
+    for ( int j = i; j >0; j--)
+    {
+        cout<<j;
+        
+    }
+   cout<<endl; 
+}
+return 0;
+}
