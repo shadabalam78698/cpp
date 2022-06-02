a042fbe47e5a38c6de78f5eeaa224657f426004e
shadabalam78698 <106025020+shadabalam78698@users.noreply.github.com>
2022-06-02 10:19:45 +0530

    Add files via upload

---
+++ pattern_11.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+cout<<"Enetr A Number"<<endl;
+cin>>n;
+for(int i=0;i<=n;i++){
+    char a='a';
+    a=a+i;
+for(int j=1;j<=n;j++){
+    cout<<a;
+
+}
+
+cout<<endl;
+}
+return 0;
+}
