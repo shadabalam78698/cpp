5295b7344c5cd80093187e9cc521cd297bc033a5
shadabalam78698 <106025020+shadabalam78698@users.noreply.github.com>
2022-06-03 22:11:56 +0530

    Update prime_number_validator.cpp

--- prime_number_validator.cpp
+++ prime_number_validator.cpp
@@ -5,18 +5,19 @@ int main(){
     int n,i;
 cout<<" Take Any Number Of Your Choice"<<endl;
 cin>>n;
-for ( i = 2; i <= n-1; i++)
+    bool divided=false;
+for ( i = 2; i < n; i++)
 {
     if (n%i==0)
     {
         cout<<"no its not a prime number"<<endl;
-        
+        divided = true;
+        break;
     }
-    else{
-        cout<<"its a prime number";
-    }
-    break;
 }
+  if(!divided){
+      cout<<"Its A Prime Number"<<endl;
+  }
 
 return 0;
 }
